d6ead97b792cf36e821fe2d97d1dd39dd146ad45
ProTerUga <104760588+ProTerUga@users.noreply.github.com>
2026-07-18 19:51:08 +0300

    Remove redundant import `org.bukkit.OfflinePlayer`

--- src/main/java/proteruga/DonationAlertsApi/SafePAPI.java
+++ src/main/java/proteruga/DonationAlertsApi/SafePAPI.java
@@ -1,12 +1,10 @@
 package proteruga.DonationAlertsApi;
 
 import me.clip.placeholderapi.PlaceholderAPI;
-import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
 
 public class SafePAPI {
     public static @NotNull String setPlaceholders(@NotNull String string) {
         return DonationAlertsApi.isPlaceholdersEnabled() ? PlaceholderAPI.setPlaceholders(null, string) : string;
     }
-
 }
